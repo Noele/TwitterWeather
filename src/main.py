bd15914e02237d48f0d53ef6404e028bce38e865
Noele <40408923+Noelee@users.noreply.github.com>
2019-07-06 07:17:44 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,22 @@
-import DataCollection.ApiRequest as APIR
-import Drawing.Graph as draw
+# Twitter Weather, Made by Noelee
+# 3rd party libs used - PIL scipy matplotlib numpy pathlib requests
 
+
+import DataCollection.ApiRequest as APIR
+import Drawing.Graph as Draw
+import OAuth.OAuthMain as OA
 class main():
     def __init__(self):
         self.self = 1
 
     def main(self):
+        """Main class to keep everything in order"""
         print("Starting \"CollectWeatherDataFromApi\" from ApiRequest.py")
-        APIR.ApiRequest.CollectWeatherDataFromApi()
-        draw.Graph.testGraph()
+        #APIR.ApiRequest.CollectWeatherDataFromApi()
+        print("Starting \"CreateGraph\" from Graph.py")
+        Draw.Graph.CreateGraph()
+        print("Starting \"OAuthSend\" from OAuthMain.py")
+        OA.OAuthMain.OAuthStart()
 
 main = main()
 main.main()
